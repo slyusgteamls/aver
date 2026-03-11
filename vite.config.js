5f8dca76e09cbff393f5dd76aa40a0fa09b2e8bb
slyusgteamls <lugalsystems@gmail.com>
2026-03-11 11:29:24 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/aver/",
   logLevel: 'error', // Suppress warnings, only show errors
   plugins: [
     base44({
